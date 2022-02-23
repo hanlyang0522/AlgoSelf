bf962cda2fe04d24bca722492a3835312b31ed6b
hanlyang0522 <500nyanpark@gmail.com>
2022-02-23 15:27:40 +0900

    11404 플로이드 업로드

--- BOJ/Graph, Tree/11404_플로이드.py
+++ BOJ/Graph, Tree/11404_플로이드.py
@@ -1,20 +1,27 @@
+# 모든 도시에서 각 도시로 가는 최소비용 --> 와샬-플로이드
+
 import sys
 
 f = sys.stdin.readline
 
 
 def floyd(n, m, bus):
-    graph = [[0 for i in range(n)] for j in range(n)]
+    graph = [[sys.maxsize if i != j else 0 for i in range(n)] for j in range(n)]
     for (a, b, c) in bus:
-        graph[a - 1][b - 1] = c
-    print(graph)
+        if c < graph[a - 1][b - 1]:
+            graph[a - 1][b - 1] = c
+    # print(graph)
 
     for k in range(n):
         for a in range(n):
             for b in range(n):
                 graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-    return graph
+    for i in range(n):
+        for j in range(n):
+            print(graph[i][j], end=" ")
+        if i != n - 1:
+            print()
 
 
 if __name__ == "__main__":
@@ -23,6 +30,5 @@ if __name__ == "__main__":
     bus = []
     for i in range(m):
         bus.append(list(map(int, f().split())))
-    print(bus)
 
-    print(floyd(n, m, bus))
+    floyd(n, m, bus)

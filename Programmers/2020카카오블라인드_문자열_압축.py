d292e780c84b9041b8bcaec6f5e89f95d65801ef
hoyang0522 <500nyanpark@gmail.com>
2022-03-22 16:01:49 +0900

    문자열압축 풀이 업로드

--- Programmers/2020카카오블라인드_문자열_압축.py
+++ Programmers/2020카카오블라인드_문자열_압축.py
@@ -1,38 +1,46 @@
 # 자르는건 앞에서부터
 # 길이는 len/2
+# i 단위로 substr 만들어서 비교
 
 
 def solution(s):
-    # print(len(s))
-    l = len(s)
-    min_len = l
+    if len(s) == 1:
+        return 1
+    min_len = len(s)
 
-    for i in range(1, l // 2 + 1):
-        tmp_s = s[:i]
-        tmp_len = i
-        repeat = False
-        # print(tmp_s)
+    for i in range(1, len(s) // 2 + 1):
+        sub_tot = ""  # 0  # ""
+        sub_str = s[:i]
+        cnt = 1
 
-        for j in range(i, l, i):
-            # print(s[j : j + i])
-            if s[j : j + i] == tmp_s:
-                if repeat == False:
-                    tmp_len += 1
-                    repeat = True
+        for j in range(i, len(s), i):
+            if sub_str == s[j : j + i]:
+                cnt += 1
+
             else:
-                tmp_s = s[j : j + i]
-                tmp_len += len(tmp_s)
-                repeat = False
+                if cnt > 1:
+                    sub_tot += str(cnt) + sub_str  # len(sub_str) + 1  #
+                else:
+                    sub_tot += sub_str  # len(sub_str)  #
+                sub_str = s[j : j + i]
+                cnt = 1
 
-        min_len = min(min_len, tmp_len)
+        if cnt > 1:
+            sub_tot += str(cnt) + sub_str  # len(sub_str) + 1  # str(cnt) + sub_str
+        else:
+            sub_tot += sub_str  # len(sub_str)  # sub_str
+
+        # print(sub_tot)
+        min_len = min(min_len, len(sub_tot))  # sub_tot)  # len(sub_tot))
 
     # print()
     print(min_len)
     return min_len
 
 
-# solution("aabbaccc")
-# solution("ababcdcdababcdcd")
+# 7 9 8 14 17
+solution("aabbaccc")
+solution("ababcdcdababcdcd")
 solution("abcabcdede")
 solution("abcabcabcabcdededededede")
 solution("xababcdcdababcdcd")

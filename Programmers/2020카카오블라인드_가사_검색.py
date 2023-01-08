c6963cda2021d820b9cbb85872f5100cc0afefff
hanlyang0522 <500nyanpark@gmail.com>
2023-01-08 21:52:45 +0900

    희성스터디 1주차

--- Programmers/2020카카오블라인드_가사_검색.py
+++ Programmers/2020카카오블라인드_가사_검색.py
@@ -1,39 +1,44 @@
+# 이중for문 돌려서 시작과 끝 찾기?? --> 효율성 탈락!
+# 그러면 bisect로 시작과 끝을 찾자!
+# ?가 앞에 있을 경우는? --> 문자 전체를 뒤집어서
+
 # 1. words 정렬
 # 2. word-query 대소비교하는 함수 작성
 # 3. query 해당하는 첫 idx ~ 끝 idx 찾기
-
+import bisect
 
 
 def binSearch(words, query, mode):
-    query_ = query.replace('?','')
+    query_ = query.replace("?", "")
     length = len(query) - len(query_)
     if length:
-        if query[0]=='?':
-            pos = 'front'
+        if query[0] == "?":
+            pos = "front"
         else:
-            pos = 'back'
-    l, r = 0, len(words)-1
+            pos = "back"
+    l, r = 0, len(words) - 1
+
 
 def wqComp(word, query_, length, pos):
-    if pos=='front':
-        if word[:len(query_)]==query_ and len(word)>=len(query_)+length:
+    if pos == "front":
+        if word[: len(query_)] == query_ and len(word) >= len(query_) + length:
             return True
     else:
-        if word[]
+        return -1
+        # if word[]
 
     return False
 
 
 def solution(words, queries):
     answer = []
-
-    words = sorted(words)
+    words = sorted(set(words))
     print(words)
 
     for query in queries:
-        start = binSearch(words, query, 'min')
-        end = binSearch(words, query, 'max')
-        answer.append(end-start)
+        start = binSearch(words, query, "min")
+        end = binSearch(words, query, "max")
+        answer.append(end - start)
 
     return answer
 

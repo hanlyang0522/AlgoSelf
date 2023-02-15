12e6a8e09699412ddc2a99cd12bfa8dc0128f5c3
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2023-02-15 15:19:52 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 336 ms, Memory: 34372 KB -BaekjoonHub

---
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
@@ -0,0 +1,75 @@
+"""
+빡구현
+"""
+
+import sys
+from collections import defaultdict
+
+f = sys.stdin.readline
+
+dirs = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
+
+
+def move():
+    global fbls
+    di = defaultdict(list)
+
+    # 파이어볼 이동
+    for k, v in fbls.items():
+        r, c = k
+        for m, s, d in v:
+            nr, nc = (r + s * dirs[d][0]) % n, (c + s * dirs[d][1]) % n
+            di[(nr, nc)].append((m, s, d))
+
+    # 파이어볼 합체
+    fbls = defaultdict(list)
+
+    # print(di)q
+    for k, v in di.items():
+        # print(k, v, len(v))
+
+        r, c = k
+        if len(v) > 1:
+            sumM, sumS = 0, 0
+            tmpD = v[0][2] % 2
+            flag = True
+            for _m, _s, _d in v:
+                sumM += _m
+                sumS += _s
+                if _d % 2 != tmpD:
+                    flag = False
+
+            # 파이어볼 분리
+            m = sumM // 5
+            if m == 0:
+                continue
+            s = int(sumS // len(v))
+            if flag:
+                d = [0, 2, 4, 6]
+            else:
+                d = [1, 3, 5, 7]
+            for __d in d:
+                fbls[(r, c)].append((m, s, __d))
+        else:
+            fbls[k].append(v[0])
+
+
+if __name__ == "__main__":
+    n, m, k = map(int, f().split())
+    fbls = defaultdict(list)
+
+    for _ in range(m):
+        r, c, m, s, d = map(int, f().split())
+        fbls[(r - 1, c - 1)].append((m, s, d))
+    # print(fbls)
+
+    for _ in range(k):
+        move()
+        # print(fbls)
+
+    total = 0
+    for k, v in fbls.items():
+        # print(v)
+        for m, s, d in v:
+            total += m
+    print(total)

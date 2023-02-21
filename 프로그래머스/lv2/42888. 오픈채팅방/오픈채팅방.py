c5eaaefc3ce66d1b82de4d507af220d6c62d551e
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2023-02-21 17:47:08 +0900

    [level 2] Title: 오픈채팅방, Time: 76.98 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,6 +1,6 @@
 def solution(record):
     di = {}
-    ans = []
+    msg = {"Enter": "님이 들어왔습니다.", "Leave": "님이 나갔습니다."}
 
     for rec in record:
         rec = rec.split()
@@ -9,14 +9,11 @@ def solution(record):
         elif rec[0] == "Change":
             di[rec[1]] = rec[2]
 
+    ans = []
     for rec in record:
         rec = rec.split()
-        if rec[0] == "Enter":
-            tmp = di[rec[1]] + "님이 들어왔습니다."
-        elif rec[0] == "Leave":
-            tmp = di[rec[1]] + "님이 나갔습니다."
-        else:
+        if rec[0] == "Change":
             continue
-        ans.append(tmp)
+        ans.append(di[rec[1]] + msg[rec[0]])
 
     return ans

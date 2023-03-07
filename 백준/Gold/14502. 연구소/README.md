9f637a7466ca1e994d472a1d4f792e1f73c196e0
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2023-03-07 22:26:30 +0900

    [Gold IV] Title: 연구소, Time: 968 ms, Memory: 125400 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/README.md
+++ 백준/Gold/14502. 연구소/README.md
@@ -1,14 +1,14 @@
-# [Gold V] 연구소 - 14502 
+# [Gold IV] 연구소 - 14502 
 
 [문제 링크](https://www.acmicpc.net/problem/14502) 
 
 ### 성능 요약
 
-메모리: 33048 KB, 시간: 4364 ms
+메모리: 125400 KB, 시간: 968 ms
 
 ### 분류
 
-너비 우선 탐색(bfs), 브루트포스 알고리즘(bruteforcing), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+너비 우선 탐색(bfs), 브루트포스 알고리즘(bruteforcing), 그래프 이론(graphs), 그래프 탐색(graph_traversal), 구현(implementation)
 
 ### 문제 설명
 
--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,71 +1,59 @@
-# 바이러스 퍼지는거 -> bfs로 처리
-# 벽 어디 세울지: 64C3 --> 최대 41664 가지
 import sys
 from itertools import combinations
+from copy import deepcopy
 from collections import deque
-import copy
+
 
 f = sys.stdin.readline
 
-dy = [1, -1, 0, 0]
-dx = [0, 0, -1, 1]
+dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
 
-def bfs(n, m, li):
-    q = deque()
-    for i in range(n):
-        for j in range(m):
-            if li[i][j] == 2:
-                q.append([i, j])
-
+def bfs(mat, virus):
+    q = deque(virus)
     while q:
         y, x = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
 
-            if 0 <= ny < n and 0 <= nx < m:
-                if li[ny][nx] == 0:
-                    li[ny][nx] = 2
-                    q.append([ny, nx])
+        for dy, dx in dirs:
+            ty, tx = y + dy, x + dx
+
+            if 0 <= ty < n and 0 <= tx < m and mat[ty][tx] == 0:
+                mat[ty][tx] = 2
+                q.append([ty, tx])
 
     cnt = 0
-    for l in li:
-        cnt += l.count(0)
+    for y in range(n):
+        for x in range(m):
+            if mat[y][x] == 0:
+                cnt += 1
+
     return cnt
 
 
-def safeSpace(n, m, li):
-    walls = list(combinations(range(n * m), 3))
-    ans = 0
-    # print(walls)
+if __name__ == "__main__":
+    n, m = map(int, f().split())
+    _mat = []
+    for _ in range(n):
+        _mat.append(list(map(int, f().split())))
 
-    for wall in walls:
-        li2 = copy.deepcopy(li)
+    virus = []
+    notSafe = 0
+    for y in range(n):
+        for x in range(m):
+            if _mat[y][x] == 2:
+                virus.append([y, x])
 
-        # 벽 설정
-        flag = True
-        for w in wall:
-            y, x = w // m, w % m
-            if li2[y][x] != 0:
-                flag = False
-                break
-            li2[y][x] = 1
-        if not flag:
+    cnt = 0
+    for w1, w2, w3 in combinations(range(n * m), 3):
+        y1, x1 = w1 // m, w1 % m
+        y2, x2 = w2 // m, w2 % m
+        y3, x3 = w3 // m, w3 % m
+
+        mat = deepcopy(_mat)
+        if mat[y1][x1] != 0 or mat[y2][x2] != 0 or mat[y3][x3] != 0:
             continue
 
-        # print(li2)
-        # 바이러스에서 BFS
-        ans = max(ans, bfs(n, m, li2))
+        mat[y1][x1] = mat[y2][x2] = mat[y3][x3] = 1
+        cnt = max(cnt, bfs(mat, virus))
 
-    return ans
-
-
-if __name__ == "__main__":
-    n, m = map(int, f().split())  # 세로, 가로
-    li = []
-    for i in range(n):
-        li.append(list(map(int, f().split())))
-
-    # print(li)
-    print(safeSpace(n, m, li))
+    print(cnt)

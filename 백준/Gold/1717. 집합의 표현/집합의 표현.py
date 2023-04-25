ec212d1f64f9243fb632f9fe126f403918f31f99
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2023-04-25 21:02:53 +0900

    [Gold V] Title: 집합의 표현, Time: 312 ms, Memory: 76720 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -9,14 +9,18 @@ sys.setrecursionlimit(1000000)
 f = sys.stdin.readline
 
 
-def find(parent, a):
+def find(a):
+    global parent
+
     if a != parent[a]:
-        parent[a] = find(parent, parent[a]) # 경로압축 하지 않을 경우 O(N^2)
+        parent[a] = find(parent[a]) # 경로압축 하지 않을 경우 O(N^2)
     return parent[a]
 
 
-def union(parent, a, b):
-    x, y = find(parent, a), find(parent, b)
+def union(a, b):
+    global parent 
+
+    x, y = find(a), find(b)
     if x < y:
         parent[y] = x
     else:
@@ -24,15 +28,17 @@ def union(parent, a, b):
 
 
 if __name__ == "__main__":
+    global parent 
+
     n, m = map(int, f().split())
     parent = [i for i in range(n + 1)]
 
     for _ in range(m):
         c, a, b = map(int, f().split())
         if c == 0:
-            union(parent, a, b)
+            union(a, b)
         else:
-            if find(parent, a) == find(parent, b):
+            if find(a) == find(b):
                 print("yes")
             else:
                 print("no")

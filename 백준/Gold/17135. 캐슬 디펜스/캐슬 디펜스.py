9b570a3454d3d1d391edfdd8d8f691a27af7e9f2
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2023-05-09 00:03:53 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 596 ms, Memory: 31832 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
@@ -1,79 +1,73 @@
+"""
+구현
+15c3 = 455 (최대 경우의 수) * 15줄 = 6825
+
+"""
+
 import sys
-import copy
+from copy import deepcopy
+from itertools import combinations
 
-def combinations(array, r):
-    for i in range(len(array)):
-        if r == 1:  # 종료 조건
-            yield [array[i]]
-        else:
-            for next in combinations(array[i + 1:], r - 1):
-                yield [array[i]] + next
 
-def attack(list_):
-    """
-    공격하기
-    거리가 D이하인 적 중에서 가장 가까운 적이고, 그러한 적이 여럿일 경우에는 가장 왼쪽에 있는 적을 공격한다.
-    - 0은 빈 칸, 1은 적
-    - 거리: |r1-r2| + |c1-c2|
-    :return:
-    """
-    # 궁수별 가까운 적 공격하기
-    attack_list = list()
-    cnt = 0
-    for l in list_:
-        pos = list()
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 1:
-                    now_d = abs(i - n) + abs(j - l)
-                    if d >= now_d:
-                        pos.append((now_d, i, j))
-        pos.sort(key=(lambda x: (x[0], x[2])))  # 거리가깝고 > 왼쪽 기준 정렬
-        if pos:
-            attack_list.append(pos[0])   # 제거해야할 적
+f = sys.stdin.readline
 
-    for a in attack_list:
-        _, i, j = a
-        if temp[i][j] == 1:
-            temp[i][j] = 0
-            cnt += 1
-    return cnt
 
-def move():
-    """
-    적 아래로 이동
-    :return:
-    """
-
-    for i in range(-1, -n, -1):
-        temp[i] = temp[i - 1]
-
-    temp[0] = [0 for _ in range(m)]
-
-def is_empty():
-    """
-    적 존재 유무
-    :return:
-    """
+def isEmpty():
     for i in range(n):
         for j in range(m):
-            if temp[i][j] == 1:
+            if mat[i][j] == 1:
                 return False
     return True
 
-n, m, d = map(int, sys.stdin.readline().split())  # 행, 열, 궁수의 공격 거리 제한
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-items = [i for i in range(m)]
-result = 0
 
-for a in combinations(items, 3):  # 1. 궁수 배치 - 모든 경우의 수 배치 3명
-    temp = copy.deepcopy(graph)
+def attack(comb):
+    attackLi = []
     count = 0
-    while not is_empty():  # 적이 있는 동안 계속 반복
-        # 2. 적 공격하기 - 가장 가까운 적 중 왼쪽에 있는
-        count += attack(a)
-        # 3. 적 아래로 이동
-        move()
-    result = max(result, count)
 
-print(result)
+    for x in comb:  # 각 궁수별 가능 타겟
+        pos = []
+
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 1:  # 가능 타겟 저장
+                    dist = abs(i - n) + abs(j - x)
+                    if d >= dist:
+                        pos.append((dist, i, j))
+
+        pos.sort(key=lambda x: (x[0], x[2]))  # 가깝고, 왼쪽순 정렬
+        if pos:
+            attackLi.append(pos[0])
+
+    # 타겟 제거
+    for _, i, j in attackLi:
+        if mat[i][j] == 1:
+            mat[i][j] = 0
+            count += 1
+
+    return count
+
+
+def move():
+    # 1칸씩 내려옴
+    for i in range(-1, -n, -1):
+        mat[i] = mat[i - 1]
+    mat[0] = [0] * m
+
+
+if __name__ == "__main__":
+    global _mat
+
+    n, m, d = map(int, f().split())
+    _mat = [list(map(int, f().split())) for _ in range(n)]
+    result = -1
+
+    for comb in combinations([i for i in range(m)], 3):
+        mat = deepcopy(_mat)
+        cnt = 0
+
+        while not isEmpty():
+            cnt += attack(comb)
+            move()
+        result = max(result, cnt)
+
+    print(result)

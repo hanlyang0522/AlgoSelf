ef6fd9ef16391dd142b5b562ab972b48c36b1895
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2025-08-18 15:24:36 +0900

    Time: 5 ms (87.92%), Space: 18.5 MB (35.02%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_englower = "".join([char for char in s if char.isalnum()]).lower()
+
+        for i in range(len(s_englower)//2):
+            if s_englower[i] != s_englower[-1 - i]:
+                return False
+
+        return True

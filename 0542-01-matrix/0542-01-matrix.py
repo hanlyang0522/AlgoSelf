ad4eba8b77849cb5a15a8261257fa8a8f46c435d
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2025-08-18 18:44:52 +0900

    Time: 290 ms (8.12%), Space: 22.7 MB (7.32%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        visited = [[float("inf")] * n for _ in range(m)]
+
+        dq = deque()
+
+        for y in range(m):
+            for x in range(n):
+                if mat[y][x] == 0:
+                    dq.append([y, x, 0])
+                    visited[y][x] = 0
+
+        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+        while dq:
+            y, x, depth = dq.popleft()
+
+            if visited[y][x] < depth:
+                continue
+
+            visited[y][x] = depth
+
+            for dy, dx in dirs:
+                ty, tx = y + dy, x + dx
+
+                if 0 <= ty < m and 0 <= tx < n:
+                    dq.append([ty, tx, depth + 1])
+
+        return visited

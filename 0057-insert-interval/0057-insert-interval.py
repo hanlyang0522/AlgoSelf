9ee06807f908eaf7f3a09cebdc76ad0b61e89c06
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2025-08-18 18:14:45 +0900

    Time: 5 ms (10.04%), Space: 19.6 MB (59.85%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,25 @@
+class Solution:
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+
+        n = len(intervals)
+        li = []
+        i = 0
+
+        # 겹치기 전까지는 그대로 추가
+        while i < n and intervals[i][1] < newInterval[0]:
+            li += [intervals[i]]
+            i += 1
+
+        # 겹친 후에는 안 겹치는거 나올 때까지 업데이트
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
+
+        # 끝났으면 니머지 그대로 추가
+        li += [newInterval]
+        li += intervals[i::]
+
+        return li

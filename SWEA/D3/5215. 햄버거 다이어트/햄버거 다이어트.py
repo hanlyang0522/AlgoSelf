506537766a95b3309012e8ef609d0f275d7e132b
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2025-11-21 15:19:31 +0900

    [D3] Title: 햄버거 다이어트, Time: 137 ms, Memory: 72,448 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, L = map(int, input().split())
+
+    food = []  # 맛, 칼로리
+
+    for _ in range(N):
+        food.append(list(map(int, input().split())))
+
+    food = sorted(food, key=lambda x: x[1])
+
+    mat = [[0] * (L + 1) for _ in range(N + 1)]
+
+    for y in range(1, N + 1):  # 재료
+        taste, cal = food[y - 1]
+
+        for x in range(1, L + 1):  # 무게
+            if x < cal:
+                mat[y][x] = max(mat[y - 1][x], mat[y][x - 1])
+            else:
+                mat[y][x] = max(mat[y - 1][x], mat[y][x - 1], mat[y - 1][x - cal] + taste)
+
+    print(f"#{tc} {mat[-1][-1]}")

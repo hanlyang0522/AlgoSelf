bf6c0c4a543e2c887be5812ab08d0af1a5934b11
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2025-11-21 15:30:03 +0900

    [D3] Title: 햄버거 다이어트, Time: 126 ms, Memory: 73,856 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -17,8 +17,8 @@ for tc in range(1, T + 1):
 
         for x in range(1, L + 1):  # 무게
             if x < cal:
-                mat[y][x] = max(mat[y - 1][x], mat[y][x - 1])
+                mat[y][x] = mat[y - 1][x]
             else:
-                mat[y][x] = max(mat[y - 1][x], mat[y][x - 1], mat[y - 1][x - cal] + taste)
+                mat[y][x] = max(mat[y - 1][x], mat[y - 1][x - cal] + taste)
 
     print(f"#{tc} {mat[-1][-1]}")

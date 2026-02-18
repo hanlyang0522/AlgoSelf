70427b6a921406358f3316ecda977cb8d2111c9e
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2026-02-18 13:47:52 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 449 ms, Memory: 5,844 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.cpp
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <set>
 #include <queue>
-#include <cmath>
+#include <vector>
+#include <cstring>
 
 using namespace std;
 
-
-vector<pair<int, int>> dirs{
-	{-1,0},{0,1},{1,0},{0,-1},
-};
-
-
 int N, M;
-vector<vector<int>> mat;
+int mat[21][21];
+int visit[21][21];
 
 typedef pair<int, int> coord;
-//const int INF = 1e9;
+coord dirs[4] = {
+	{-1,0}, {0,1}, {1,0}, {0,-1}
+};
+
 
 void init()
 {
 	cin >> N >> M;
+	memset(mat, 0, sizeof(mat));
 
-	mat.assign(N, vector<int>(N, 0));
-
-	for (int y = 0; y < N; ++y)
-		for (int x = 0; x < N; ++x)
+	for (int y = 0;y < N;++y)
+		for (int x = 0;x < N;++x)
 			cin >> mat[y][x];
 }
 
@@ -39,57 +34,66 @@ bool isValid(int y, int x)
 }
 
 
-
 int bfs(int y, int x)
 {
-	vector<vector<bool>> visit;
-	visit.assign(N, vector<bool>(N, false));
+	int home_cnt = 0;
+	int home_max = 0;
+	int cost = 0;
+	int rds = -1;
 
 	queue<coord> q;
 	q.push({ y,x });
-	visit[y][x] = true;
 
-	int home_cnt = mat[y][x];
-	int home_max = 0;
-	int cost = 0;
-	int r = 0;
+	memset(visit, 0, sizeof(visit));
+	visit[y][x] = 1;
 
-	while (!q.empty()) {
-		// 서비스 가능한지 계산
-		r += 1;
-		cost = r * r + (r - 1) * (r - 1);
+	//if (mat[y][x])
+	//	home_cnt += 1;
+
+	while (!q.empty())
+	{
+		rds += 1;
+		cost = rds * rds + (rds - 1) * (rds - 1);
 
 		if (home_cnt * M >= cost)
 			home_max = home_cnt;
+		
+		int it = q.size();
+		//cout << "rds: " << rds << " cost: " << cost <<"home_cnt: "<<home_cnt<< " it: " << it<<"\n";
 
-		int q_size = q.size();
-
-		for (int i = 0; i < q_size; ++i) {
-			coord curr = q.front(); q.pop();
+		for (int i = 0;i < it;++i)
+		{
+			coord curr = q.front();q.pop();
 			int cy = curr.first;
 			int cx = curr.second;
 
-			for (const auto& d : dirs) {
+			if (mat[cy][cx]) {
+				home_cnt += 1;
+				//cout << "DEBUG: " << cy << " " << cx << " \n";
+			}
+
+			for (const auto& d : dirs)
+			{
 				int ny = cy + d.first;
 				int nx = cx + d.second;
 
 				if (!isValid(ny, nx)) continue;
 				if (visit[ny][nx]) continue;
 
-				if (mat[ny][nx] == 1)	// home update
-					home_cnt += 1;
-
-				visit[ny][nx] = true;
+				visit[ny][nx] = 1;
 				q.push({ ny,nx });
 			}
 		}
 	}
 
+	rds += 1;
+	cost = rds * rds + (rds - 1) * (rds - 1);
+
+	if (home_cnt * M >= cost)
+		home_max = home_cnt;
+
 	return home_max;
 }
-
-
-
 
 int solve()
 {
@@ -97,30 +101,22 @@ int solve()
 
 	int ans = -1;
 
-	for (int y = 0; y < N; ++y) {
-		for (int x = 0; x < N; ++x) {
-
-			//y = 3, x = 3;
+	for (int y = 0;y < N;++y)
+		for (int x = 0;x < N;++x) {
+			//y = 10, x = 10;
 			ans = max(ans, bfs(y, x));
 		}
-	}
 
 	return ans;
 }
 
-
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
 	int tc;
 	cin >> tc;
 
-	for (int i = 1; i <= tc; ++i)
-	{
+	for (int i = 1;i <= tc;++i)
 		cout << "#" << i << " " << solve() << "\n";
-	}
+
 	return 0;
 }

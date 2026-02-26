e4fa3eff677e5d65c051cdcb017afe4bc9f6e9ce
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2026-02-26 16:39:36 +0900

    Title: 사각형 채우기 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260226/사각형 채우기 3/rectangle-fill-3.cpp
+++ 260226/사각형 채우기 3/rectangle-fill-3.cpp
@@ -6,34 +6,47 @@ using namespace std;
 
 
 int N;
-vector<int> vi;
+vector<int> dp;
 
 void init()
 {
     cin >> N;
-    vi.assign(N + 1, -1);
+    dp.assign(N + 1, -1);
 
-    vi[0] = 1;
-    vi[1] = 2;
-    vi[2] = 7;
+    //vi[0] = 1;
+    //vi[1] = 2;
+    //vi[2] = 7;
 }
 
-int dp(int n)
+int doDP(int n)
 {
-    if (vi[n] != -1)
-        return vi[n];
+ //   if (vi[n] != -1)
+ //       return vi[n];
 
-    //vi[n] = (dp(n - 1) + dp(n - 2)) % 1000000007;
-	vi[n] = (dp(n - 3) * 2 + dp(n - 2) * 3 + dp(n - 1) * 2) % 1000000007;
+ //   //vi[n] = (dp(n - 1) + dp(n - 2)) % 1000000007;
+	//vi[n] = (2 + dp(n - 2) * 3 + dp(n - 1) * 2) % 1000000007;
 
-    return vi[n];
+ //   return vi[n];
+
+    dp[0] = 1;
+    dp[1] = 2;
+
+    for (int i = 2; i <= N; ++i)
+    {
+		dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3;
+
+        for (int j = i - 3; j >= 0; --j)
+            dp[i] += dp[j] * 2;
+	}
+
+    return dp[N];
 }
 
 int solve()
 {
     init();
 
-    return dp(N);
+    return doDP(N);
 }
 
 

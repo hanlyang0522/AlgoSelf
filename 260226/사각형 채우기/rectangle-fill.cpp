723a9231e6b75f63cb5419d6f191e56ed11d770c
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2026-02-26 14:11:18 +0900

    Title: 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260226/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+int N;
+vector<int> vi;
+
+void init()
+{
+    cin >> N;
+    vi.assign(N + 1, -1);
+
+    vi[0] = 0;
+    vi[1] = 1;
+    vi[2] = 2;
+}
+
+int dp(int n)
+{
+    if (vi[n] != -1)
+        return vi[n];
+
+    vi[n] = dp(n - 1) + dp(n - 2);
+
+    return vi[n];
+}
+
+int solve()
+{
+    init();
+
+    return dp(N);
+}
+
+
+int main() {
+    cout << solve();
+    int k = 3;
+    return 0;
+}

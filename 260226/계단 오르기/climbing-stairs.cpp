e51816f1b54169758201e3dcbd2b6c09c1f433c4
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2026-02-26 12:12:58 +0900

    Title: 계단 오르기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260226/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int N;
+vector<int> vi;
+
+void init()
+{
+    cin >> N;
+    vi.assign(N + 1, -1);
+
+    vi[0] = 0;
+    vi[1] = 0;
+    vi[2] = 1;
+    vi[3] = 1;
+}
+
+
+int step(int n)
+{
+    if (vi[n] != -1)
+        return vi[n];
+
+	vi[n] = min(step(n - 2) + 1, step(n - 3) + 1);
+
+    return  vi[n];
+}
+
+
+int solve()
+{
+    init();
+
+    return step(N)%10007;
+}
+
+
+int main() {
+    cout << solve();
+    return 0;
+}

4bb7b3f8b8205d1eee86e9fd1f72c8c0423cbfb2
h.lyyan_g <67934041+hanlyang0522@users.noreply.github.com>
2026-04-03 21:45:04 +0900

    [level 3] Title: 합승 택시 요금, Time: 27.23 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,57 +1,48 @@
-from collections import defaultdict, deque
+from collections import defaultdict
+from heapq import heappop, heappush
 
-INF = 100000*201
-    
+
+def dijkstra(n, graph, start):
+    dist = {node: int(1e9) for node in range(1, n + 1)}
+    dist[start] = 0
+
+    q = []
+    heappush(q, [dist[start], start])  # 비용, node 순 입력
+
+    while q:
+        curDist, curNode = heappop(q)
+
+        if dist[curNode] < curDist:  # 기존 경로보다 비싸면 skip
+            continue
+
+        for newNode, newDist in graph[curNode].items():  # dict에선 node:비용으로 나옴
+            di = curDist + newDist
+            if di < dist[newNode]:  # 기존 경로보다 싸면 갱신
+                dist[newNode] = di
+                heappush(q, [di, newNode])
+    return dist
+
 
 def solution(n, s, a, b, fares):
-    li =[[INF]*(n+1) for _ in range(n+1)]
-    adj_li = defaultdict(list)
-    
-    for c,d,f in fares:
-        li[c][d] = f
-        li[d][c] = f
-        adj_li[c].append([d,f])
-        adj_li[d].append([c,f])
-    
-    for i in range(n+1):
-        li[i][i] = 0
-    
-    # 1. w-f로 모든 node-node weight 구하기
-    for k in range(n+1):
-        for n1 in range(n+1):
-            for n2 in range(n+1):
-                li[n1][n2] = min(li[n1][n2], li[n1][k]+li[k][n2])
-                
-    # debug
-#    for i in range(1, n+1):
-#        for j in range(1, n+1):
-#            print(li[i][j], end=' ')
-#        print()   
-    
-    
-    
-    # 2. bfs 돌면서 최적해 업데이트
-    dq = deque()
-    dq.append([s, 0])
-    
-    ans = li[s][a]+li[s][b]
-    visit = [INF]*(n+1)
-    visit[s] = 0
-    
-    while dq:
-        curr, cost = dq.pop()
-        
-        # 방문시 합승~a,b 비용 계산
-        tmp_cost = cost + li[curr][a] + li[curr][b]    
-        ans = min(ans, tmp_cost)
-        #print(curr, cost, tmp_cost)
-        
-        for nxt, w in adj_li[curr]:
-            if visit[nxt] < cost+w:
-                continue
-            
-            visit[nxt] = cost+w
-            dq.append([nxt, cost+w])
-    
-    
+    # graph 생성
+    graph = defaultdict(dict)
+    for c, d, f in fares:
+        graph[c][d] = f
+        graph[d][c] = f
+    # print(graph)
+
+    # 다익스트라로 각 node간 최단 거리를 구함
+    shortPath = {}
+    for i in [s, a, b]:
+        shortPath[i] = dijkstra(n, graph, i)
+    # print(shortPath)
+
+    # 각 node별 분기점일 경우의 최단 거리 구함
+    ans = int(1e9)
+    for i in range(1, n + 1):
+        ans = min(
+            ans,
+            (shortPath[s][i] + shortPath[a][i] + shortPath[b][i]),
+        )
+
     return ans
